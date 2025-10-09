8b50737eda53354b5136935af0330b040fa8c12a
MP-XXL <royceapera@yahoo.com>
2025-10-09 15:45:01 -0500

    Modifes test_image.py

--- Day02/test_image/test_image.py
+++ Day02/test_image/test_image.py
@@ -1,7 +1,20 @@
+class EmpytInputError(Exception):
+    pass
+
 def hello(prompt):
-    user_input = input(prompt).capitalize()
-    print(f"Hello, {user_input}")
-    print("Stay jiggy!")
+    while True:
+        user_input = input(prompt).capitalize()
+        try:
+            if user_input == "":
+                raise EmpytInputError
+            else:
+                print(f"Hello, {user_input}")
+                print("Stay jiggy!")
+                return f"Goodbye {user_input}"
+        except EmpytInputError:
+            print("User did not enter a valid input!")
+        except Exception:
+            print("Oops! Something went wrong!")
 
 
-command = hello("Enter your name : ")
+command = print(hello("Enter your name : "))
