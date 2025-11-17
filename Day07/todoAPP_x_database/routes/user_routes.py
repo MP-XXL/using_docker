3d1e8f521c7aec97b43c4820f43c91698c348857
MP-XXL <royceapera@yahoo.com>
2025-11-17 03:32:28 -0600

    Feat: Adds CRUD for users ALchemy

--- Day07/todoAPP_x_database/routes/user_routes.py
+++ Day07/todoAPP_x_database/routes/user_routes.py
@@ -4,6 +4,7 @@ from database_models.data_models import User, UserResponse
 from database_models import db_model
 from fastapi import APIRouter, Depends, HTTPException, status
 from datetime import datetime
+from typing import List
 
 router = APIRouter()
 
@@ -30,4 +31,46 @@ def create_user(user: User, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
     
-    
+@router.get("/users/{user_id}", response_model=UserResponse)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    user = db.query(db_model.User).filter(db_model.User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found!"
+        )
+    return user
+
+@router.put("/users/{user_id}", response_model=UserResponse)
+def update_user(user_id: int, user: User, db: Session = Depends(get_db)):
+    updated_user = db.query(db_model.User).filter(db_model.User.id == user_id).first()
+    if not updated_user:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found!"
+        )
+    for field, value in user.dict().items():
+        setattr(updated_user, field, value)
+    db.commit()
+    db.refresh(updated_user)
+    return updated_user
+
+@router.delete("/users/{user_id}", response_model=UserResponse)
+def delete_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = db.query(db_model.User).filter(db_model.User.id == user_id).first()
+    if not db_user:
+        raise HTTPException(
+            status_code = status.HTTP_404_NOT_FOUND,
+            detail = "User not found!"
+        )
+    db.delete(db_user)
+    db.commit()
+    raise HTTPException(
+        status_code = status.HTTP_204_NO_CONTENT,
+        detail = "Deleted user"
+    )
+
+@router.get("/users", response_model=List[UserResponse])
+def get_all_users(db: Session = Depends(get_db)):
+    return db.query(db_model.User).all()
+ 
